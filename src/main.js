3c203fd0477a5efa734e107e20d2719b15a65984
Luisa006 <lufermartinez0618@gmail.com>
2022-02-09 12:51:44 -0500

    Todos los botones filtran roles

--- src/main.js
+++ src/main.js
@@ -4,11 +4,38 @@ const todaData = Object.values(data.data); //obtener todos los valores de los su
 const seeAllChampions = () =>{
     showData(todaData);
 }
+
+const seeAssassinChampions = () =>{
+    let arrayAssassin = todaData.filter(champion => champion.tags.includes("Assassin"));
+        showData(arrayAssassin); 
+}
+
+const seeFighterChampions = () =>{
+    let arrayFighter = todaData.filter(champion => champion.tags.includes("Fighter"));
+        showData(arrayFighter); 
+}
+
+const seeMageChampions = () =>{
+    let arrayMage = todaData.filter(champion => champion.tags.includes("Mage"));
+        showData(arrayMage); 
+}
+
+const seeSupChampions = () =>{
+    let arraySup = todaData.filter(champion => champion.tags.includes("Support"));
+        showData(arraySup); 
+}
+
 const seeTankchampions = () =>{
     let arrayTanks = todaData.filter(champion => champion.tags.includes("Tank"));
-    console.log(arrayTanks);
-    showData(arrayTanks);
+        showData(arrayTanks); 
 }
+
+const seeMarkchampions = () =>{
+    let arrayMark = todaData.filter(champion => champion.tags.includes("Marksman"));
+        showData(arrayMark); 
+}
+
+
 const showData = (array) => {
     document.getElementById("root").innerHTML="";
     array.forEach((champion) => {
@@ -20,5 +47,9 @@ const showData = (array) => {
     });
 }
 document.querySelector("#allChampions").addEventListener("click", seeAllChampions);
+document.querySelector("#assassinChampions").addEventListener("click", seeAssassinChampions);
+document.querySelector("#fighterChampions").addEventListener("click", seeFighterChampions);
+document.querySelector("#mageChampions").addEventListener("click", seeMageChampions);
+document.querySelector("#supChampions").addEventListener("click", seeSupChampions);
 document.querySelector("#tankChampions").addEventListener("click", seeTankchampions);
-document.querySelector("#magicChampions").addEventListener("click", seeAllChampions);
+document.querySelector("#marksmanChampions").addEventListener("click", seeMarkchampions);
