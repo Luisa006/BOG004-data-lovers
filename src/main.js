d2126d3a64a9917f955b1f6b0c6057b52ea947e4
Maria Saavedra <cotesaavedra@github.com>
2022-02-09 14:04:44 -0300

    funcionalidad a los botones para filtrar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
-console.log(example, data);
+import data from './data/lol/lol.js';
+const todaData = Object.values(data.data); //obtener todos los valores de los subobjetos (objetos que están dentro del objeto)
+const seeAllChampions = () =>{
+    showData(todaData);
+}
+const seeTankchampions = () =>{
+    let arrayTanks = todaData.filter(champion => champion.tags.includes("Tank"));
+    console.log(arrayTanks);
+    showData(arrayTanks);
+}
+const showData = (array) => {
+    document.getElementById("root").innerHTML="";
+    array.forEach((champion) => {
+    let newTitle = document.createElement("h2");
+    let newContent = document.createTextNode(champion.name);
+    newTitle.appendChild(newContent);
+    let currentDiv = document.getElementById("root");
+    currentDiv.insertBefore(newTitle, currentDiv.parentNode[0]);
+    });
+}
+document.querySelector("#allChampions").addEventListener("click", seeAllChampions);
+document.querySelector("#tankChampions").addEventListener("click", seeTankchampions);
+document.querySelector("#magicChampions").addEventListener("click", seeAllChampions);
