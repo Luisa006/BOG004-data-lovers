7c2de54e3f0d98bdc7aacc1940d828c20fdb4471
Luisa006 <lufermartinez0618@gmail.com>
2022-02-21 10:20:54 -0500

    activar botones

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import data from './data/lol/lol.js';
-import { seeAssassinChampions, seeFighterChampions, seeMageChampions, seeSupChampions, seeTankchampions, seeMarkchampions, orderDifficulty,  } from './data.js';
+import { seeAssassinChampions, seeFighterChampions, seeMageChampions, seeSupChampions, seeTankchampions, seeMarkchampions, orderDifficulty, orderName } from './data.js';
 const todaData = Object.values(data.data);
 const nameData = Object.keys(data.data); //array con los nombres
-console.log (nameData);
+console.log(nameData);
 console.log(todaData);
 const showData = (array) => {
     document.getElementById("root").innerHTML = "";
@@ -18,35 +18,69 @@ const showData = (array) => {
     });
 }
 showData(todaData);
+const activeButton = (button) => {
+    allChampions.style.backgroundColor = "transparent";
+    assassinChampions.style.backgroundColor = "transparent";
+    fighterChampions.style.backgroundColor = "transparent";
+    mageChampions.style.backgroundColor = "transparent";
+    supChampions.style.backgroundColor = "transparent";
+    tankChampions.style.backgroundColor = "transparent";
+    marksmanChampions.style.backgroundColor = "transparent";
+    button.style.backgroundColor = "#00aae4";
+}
+const orderButtons = (button) => {
+    orderDif.style.backgroundColor = "transparent";
+    orderNamebtn.style.backgroundColor = "transparent";
+    allChampions.style.backgroundColor = "#00aae4";
+    assassinChampions.style.backgroundColor = "transparent";
+    fighterChampions.style.backgroundColor = "transparent";
+    mageChampions.style.backgroundColor = "transparent";
+    supChampions.style.backgroundColor = "transparent";
+    tankChampions.style.backgroundColor = "transparent";
+    marksmanChampions.style.backgroundColor = "transparent";
+    button.style.backgroundColor = "#00aae4";
+
+}
+
 document.querySelector("#allChampions").addEventListener("click", () => {
     showData(todaData);
+    activeButton(allChampions);
 });
 document.querySelector("#assassinChampions").addEventListener("click", () => {
     showData(seeAssassinChampions(todaData));
+    activeButton(assassinChampions);
 });
 document.querySelector("#fighterChampions").addEventListener("click", () => {
     showData(seeFighterChampions(todaData));
+    activeButton(fighterChampions);
 });
+
 document.querySelector("#mageChampions").addEventListener("click", () => {
     showData(seeMageChampions(todaData));
+    activeButton(mageChampions);
 });
+
 document.querySelector("#supChampions").addEventListener("click", () => {
     showData(seeSupChampions(todaData));
+    activeButton(supChampions);
 });
+
 document.querySelector("#tankChampions").addEventListener("click", () => {
     showData(seeTankchampions(todaData));
-    // document.getElementsByClassName("boton").innerHTML = "";
-    // tankChampions.style.backgroundColor="#00aae4";
+    activeButton(tankChampions);
 });
+
 document.querySelector("#marksmanChampions").addEventListener("click", () => {
     showData(seeMarkchampions(todaData));
-    // document.getElementsByClassName("boton").innerHTML = "";
-    // marksmanChampions.style.backgroundColor="#00aae4";
+    activeButton(marksmanChampions);
 });
 document.querySelector("#orderDif").addEventListener("click", () => {
     showData(orderDifficulty(todaData));
+    orderButtons(orderDif);
 });
 
-document.querySelector("#orderName").addEventListener("click", () => {
+document.querySelector("#orderNamebtn").addEventListener("click", () => {
     showData(orderName(todaData));
+    orderButtons(orderNamebtn);
+
 });
