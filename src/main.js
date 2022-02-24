46fd252a64297dccd7a157b19cbf6bf373a717d0
Luisa006 <lufermartinez0618@gmail.com>
2022-02-24 11:22:45 -0500

    media query cajas botones

--- src/main.js
+++ src/main.js
@@ -51,13 +51,14 @@ document.querySelector("#assassinChampions").addEventListener("click", () => {
     showData(seeAssassinChampions(todaData));
     activeButton("assassinChampions");
     let totalChamps = seeAssassinChampions(todaData);
-    document.getElementById("textPerc").innerText = `el ${percentage(totalChamps)}% de los campeones son `;
+    document.getElementById("textPerc").innerText = `el ${percentage(totalChamps)}% de los campeones son ASESINOS, estos se especializan en matar o desestabilizar objetivos de alto valor estratégico. Orientados a la infiltración, engaño, y movimiento, son cazadores oportunistas que buscan el momento favorable antes de entrar al combate.
+    `;
 });
 document.querySelector("#fighterChampions").addEventListener("click", () => {
     showData(seeFighterChampions(todaData));
     activeButton("fighterChampions");
     let totalChamps = seeFighterChampions(todaData);
-    document.getElementById("textPerc").innerText = `el ${percentage(totalChamps)}% de los campeones son `;
+    document.getElementById("textPerc").innerText = `el ${percentage(totalChamps)}% de los campeones son LUCHADORES, tambien conocidos como Guerreros) son un grupo diverso de combatientes de corto alcance que sobresalen tanto causando como resistiendo daño. `;
 });
 
 document.querySelector("#mageChampions").addEventListener("click", () => {
@@ -65,28 +66,28 @@ document.querySelector("#mageChampions").addEventListener("click", () => {
     activeButton("mageChampions");
     let totalChamps = seeMageChampions(todaData);
     percentage(totalChamps);
-    document.getElementById("textPerc").innerText = `el ${percentage(totalChamps)}% de los campeones son `;
+    document.getElementById("textPerc").innerText = `el ${percentage(totalChamps)}% de los campeones son MAGOS, personajes enfocados en el gran alcance por norma general, que además poseen capacidades de daño en área gracias a sus habilidades y control de masas. `;
 });
 
 document.querySelector("#supChampions").addEventListener("click", () => {
     showData(seeSupChampions(todaData));
     activeButton("supChampions");
     let totalChamps = seeSupChampions(todaData);
-    document.getElementById("textPerc").innerText = `el ${percentage(totalChamps)}% de los campeones son `;
+    document.getElementById("textPerc").innerText = `el ${percentage(totalChamps)}% de los campeones son SOPORTES, se encargan de reforzar y/o apoyar a sus compaañeros de equipo mediante la utilidad brindada por sus bonificaciones, curaciones, escudos, inmovilizaciones a los enemigos y cualquier otro control de masas existente.`;
 });
 
 document.querySelector("#tankChampions").addEventListener("click", () => {
     showData(seeTankchampions(todaData));
     activeButton("tankChampions");
     let totalChamps = seeTankchampions(todaData);
-    document.getElementById("textPerc").innerText = `el ${percentage(totalChamps)}% de los campeones son `;
+    document.getElementById("textPerc").innerText = `el ${percentage(totalChamps)}% de los campeones son TANQUES, campeones de combate cuerpo a cuerpo que sacrifican el daño a cambio de un poderoso control de masas. Las subcategorías de tanques son Vanguardias y Protectores. `;
 });
 
 document.querySelector("#marksmanChampions").addEventListener("click", () => {
     showData(seeMarkchampions(todaData));
     activeButton("marksmanChampions");
     let totalChamps = seeMarkchampions(todaData);
-    document.getElementById("textPerc").innerText = `el ${percentage(totalChamps)}% de los campeones son `;
+    document.getElementById("textPerc").innerText = `el ${percentage(totalChamps)}% de los campeones son TIRADORES, también llamados ADC (Attack Damage Carry, por infligir principalmente daño físico) parte desde la calle inferior y exige tanto un gran conocimiento del rol como cierta habilidad mecánica, especialmente en lo que al dominio del 'kiteo' se refiere. `;
 });
 document.querySelector("#orderDif").addEventListener("click", () => {
     showData(orderDifficulty(todaData));
